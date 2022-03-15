a8c1efabf4b5f6c6f4c9bc1ca78105ad49083729
Sara <saritagm8@gmail.com>
2022-03-15 13:04:21 +0100

    nivel 2 creado  14/03

--- Tarea-S0303-Patterns-Team-main/s0303TeamPatterns/src/Nivel2/conectaBD_SQL/ConnectionDB_SQL.java
+++ Tarea-S0303-Patterns-Team-main/s0303TeamPatterns/src/Nivel2/conectaBD_SQL/ConnectionDB_SQL.java
@@ -5,24 +5,31 @@ public class ConnectionDB_SQL {
     public static void main(String[]args) {
 
         final String CONTROLADOR = "com.mysql.cj.jdbc.Driver";
-        final String URL = "jdbc:mysql://localhost:3306/mysql";
+        final String URL = "jdbc:mysql://localhost:3306/floristeria";
         final String USUARIO = "root";
         final String CLAVE = "Sg7535sg";
 
         try {
             //crear conexion
             Connection myConnection = DriverManager.getConnection(URL, USUARIO, CLAVE);
+            System.out.println("Connectando.....");
 
             //crear objeto statment
             Statement myStatement = myConnection.createStatement();
+            System.out.println("Connectado con éxito");
 
-            String instruccionSql = "INSERT INTO ARTICULO (IDARTICULO, NOMBRE, PRECIO) VALUES ('','','')";
+            String instruccionTable = "CREATE TABLE Floristeria (nombre VARCHAR(20)) ";
+            String instruccionSql1 = "INSERT INTO floristeria (NOMBRE) VALUES ('La Flor')";
+
+            String instruccionSql = "INSERT INTO ARTICULO (IDARTICULO, NOMBRE, PRECIO) VALUES ('A1','pino','3.9')";
+
+
+            myStatement.execute(instruccionSql1);
+            System.out.println("Datos inseridos correctamente");
 
 
 /*
-            AppMenu appMenu = new AppMenu();
 
-            appMenu.realizarOpcionMenu();
             //ejecutar la instruccion sql
             //ResultSet myResulSet = myStatement.executeQuery();  ---> aqui dentro va la query SELECT * FROM ARTICULO
             //recorrer el resulset
